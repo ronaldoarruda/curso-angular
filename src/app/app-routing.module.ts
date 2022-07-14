df685e5f57e3a17d4685dc6d7d82832f65aa642f
Ronaldo Arruda <nronaldo_7@hotmail.com>
2022-07-14 16:04:03 -0300

    requisição http do array de animais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { EventosComponent } from './components/eventos/eventos.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'event', component: EventosComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
